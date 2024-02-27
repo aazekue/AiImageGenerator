fc3706076bf286e056ab4e2a2a4993722e53f43c
aazekue <aazekue@gmail.com>
2024-02-27 15:12:13 -0500

    Initial Commit

---
+++ AiImageGenerator/AiImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiImageGeneratorApp.swift
+//  AiImageGenerator
+//
+//  Created by Abel Azekwu on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AiImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
